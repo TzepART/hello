b31fc9de380bb5e5fc633644cb387b7f3aea7943
tzepart <tzepart@gmail.com>
2017-10-09 01:40:54 +0300

    w_add_using_join

--- src/test/severalThreads/MoreThreads.java
+++ src/test/severalThreads/MoreThreads.java
@@ -4,6 +4,13 @@ public class MoreThreads {
     public static void main(String args[]){
         System.out.println("On main thread");
 
+//        severalThreads();
+        severalThreadsWithJoin();
+
+        System.out.println("Off main thread");
+    }
+
+    private static void severalThreads() {
         MyThread mt1 = new MyThread("Child #1");
         MyThread mt2 = new MyThread("Child #2");
         MyThread mt3 = new MyThread("Child #3");
@@ -22,7 +29,25 @@ public class MoreThreads {
                 mt2.thread.isAlive() ||
                 mt3.thread.isAlive()
         );
+    }
 
-        System.out.println("Off main thread");
+    private static void severalThreadsWithJoin() {
+        MyThread mt1 = new MyThread("Child #1");
+        MyThread mt2 = new MyThread("Child #2");
+        MyThread mt3 = new MyThread("Child #3");
+
+        try{
+            mt1.thread.join();
+            System.out.println("Child #1 - connect");
+
+            mt2.thread.join();
+            System.out.println("Child #2 - connect");
+
+            mt3.thread.join();
+            System.out.println("Child #3 - connect");
+
+        }catch (InterruptedException exc){
+            System.out.println("Crash main thread");
+        }
     }
 }
