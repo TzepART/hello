93c1bda25068b0bcdb60639e950ffb88e33718da
tzepart <tzepart@gmail.com>
2017-10-27 17:59:57 +0300

    w_task4_step1

---
+++ src/task/task4/UseThreads.java
@@ -0,0 +1,9 @@
+package task.task4;
+
+public class UseThreads {
+
+    public static void main(String[] args) {
+        Teacher producer = new Teacher();
+        Student consumer = new Student();
+    }
+}
