473f70ba9681f79e1338056327fc26400503b0e4
tzepart <tzepart@gmail.com>
2017-10-27 13:39:45 +0300

    w_fix

--- src/task/task3/FirstTread.java
+++ src/task/task3/FirstTread.java
@@ -4,7 +4,7 @@ public class FirstTread implements Runnable {
 
     Thread thread;
     private IncrementClass incrementObj;
-    static Integer count = 5;
+    static Integer count = 15;
 
     public FirstTread(String name, IncrementClass incrementObj) {
         this.thread = new Thread(this, name);
@@ -14,6 +14,7 @@ public class FirstTread implements Runnable {
     @Override
     public void run(){
         for (int i = 1; i <= count; i++) {
+            System.out.print("Поток - "+this.thread.getName()+"; ");
             incrementObj.inc10(i);
         }
     }
--- src/task/task3/SecondThread.java
+++ src/task/task3/SecondThread.java
@@ -3,15 +3,17 @@ package task.task3;
 class SecondThread extends Thread{
 
     private IncrementClass incrementObj;
-    static Integer count = 10;
+    static Integer count = 5;
 
     public SecondThread(String name, IncrementClass incrementObj) {
+        super(name);//set Name to thread
         this.incrementObj = incrementObj;
     }
 
     @Override
     public void run(){
         for (int i = 1; i <= count; i++) {
+            System.out.print("Поток - "+this.getName()+"; ");
             incrementObj.inc100(i);
         }
     }
