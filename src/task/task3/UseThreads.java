89e81443dd4ac6c2fcd199f3f5e8b9d6a4b483cb
tzepart <tzepart@gmail.com>
2017-10-28 12:51:28 +0300

    w_fix

--- src/task/task3/UseThreads.java
+++ src/task/task3/UseThreads.java
@@ -4,10 +4,11 @@ package task.task3;
 public class UseThreads {
 
     public static void main(String args[]){
-
+        Integer countFirst = Integer.parseInt(args[0]);
+        Integer countSecond = Integer.parseInt(args[1]);
         IncrementClass incrementObj = new IncrementClass();
-        FirstTread mt1 = new FirstTread("FirstTread", incrementObj, 12);
-        SecondThread mt2 = new SecondThread("SecondThread", incrementObj, 10);
+        FirstTread mt1 = new FirstTread("FirstTread", incrementObj, countFirst);
+        SecondThread mt2 = new SecondThread("SecondThread", incrementObj, countSecond);
 
         mt2.start();
         mt1.thread.start();
