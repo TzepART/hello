0a5e282a799b2be290856e6e0e2e306342ea2ecf
tzepart <tzepart@gmail.com>
2017-11-01 02:29:14 +0300

    w_add_sql_query_STEP2

--- src/test/genarateSql/sql/task.sql
+++ src/test/genarateSql/sql/task.sql
@@ -61,14 +61,56 @@ SELECT count(*) as count_not_return_books FROM library.journal as journal WHERE 
 --3. Если количество дней положительно то добавляем в выборку + сумма за просрочку за день
 --4. Пермножить дни на сумму
 --5. Получить общую сумму
+SELECT SUM(book_types.fine*books_clients.day_diff) as full_fine FROM library.book_types as book_types inner join (
+    SELECT DATEDIFF(IF(journal.date_return_real <> '0000-00-00 00:00:00', journal.date_return_real, CURDATE()),journal.date_return) AS day_diff,
+        IF(journal.date_return_real <> '0000-00-00 00:00:00', journal.date_return_real, CURDATE()) as date_return_upd,
+         books.type_id as book_type_id
+     FROM library.journal as journal inner join library.books as books on journal.book_id = books.id where client_id='10'
+) as books_clients on book_types.id = books_clients.book_type_id where books_clients.day_diff > 0 order by full_fine DESC;
+
 
 /*Размер самого большого штрафа*/
 --1. Получить все записи журнала для пользователя
 --2. Вычислить разницу по времени между (если вернул, то врменем возращения, если нет то сегодняшней датой) и временем когда нужно вернуть
---3. Если количество дней положительно то добавляем в выборку + сумма за просрочку за день
---4. Пермножить дни на сумму
+SELECT IF(journal.date_return_real <> '0000-00-00 00:00:00', journal.date_return_real, CURDATE()) as date_return_upd,
+         journal.book_id as book_id,
+         journal.client_id as client_id,
+         journal.ddate as ddate,
+         journal.date_return as date_return
+     FROM library.journal as journal where journal.client_id='8';
+--3. Разницу дней между временем когда нужно было сдать и когда сдал + добавляем информацию о книге
+SELECT DATEDIFF(IF(journal.date_return_real <> '0000-00-00 00:00:00', journal.date_return_real, CURDATE()),journal.date_return) AS day_diff,
+        IF(journal.date_return_real <> '0000-00-00 00:00:00', journal.date_return_real, CURDATE()) as date_return_upd,
+         books.id as book_id,
+         books.name as book_name,
+         books.cnt as book_cnt,
+         books.type_id as book_type_id,
+         journal.client_id as client_id,
+         journal.ddate as ddate,
+         journal.date_return as date_return
+     FROM library.journal as journal inner join library.books as books on journal.book_id = books.id where client_id='10';
+--4. Если количество дней положительно то добавляем в выборку + сумма за просрочку за день
+SELECT * FROM library.book_types as book_types inner join (
+    SELECT DATEDIFF(IF(journal.date_return_real <> '0000-00-00 00:00:00', journal.date_return_real, CURDATE()),journal.date_return) AS day_diff,
+        IF(journal.date_return_real <> '0000-00-00 00:00:00', journal.date_return_real, CURDATE()) as date_return_upd,
+         books.id as book_id,
+         books.name as book_name,
+         books.cnt as book_cnt,
+         books.type_id as book_type_id,
+         journal.client_id as client_id,
+         journal.ddate as ddate,
+         journal.date_return as date_return
+     FROM library.journal as journal inner join library.books as books on journal.book_id = books.id where client_id='10'
+) as books_clients on book_types.id = books_clients.book_type_id where books_clients.day_diff > 0;
+--5. Пермножить дни на сумму
 --6. Сортируем
 --7. Береем верхнее значение
+SELECT book_types.fine*books_clients.day_diff as full_fine, book_types.fine, books_clients.day_diff FROM library.book_types as book_types inner join (
+    SELECT DATEDIFF(IF(journal.date_return_real <> '0000-00-00 00:00:00', journal.date_return_real, CURDATE()),journal.date_return) AS day_diff,
+        IF(journal.date_return_real <> '0000-00-00 00:00:00', journal.date_return_real, CURDATE()) as date_return_upd,
+         books.type_id as book_type_id
+     FROM library.journal as journal inner join library.books as books on journal.book_id = books.id where client_id='10'
+) as books_clients on book_types.id = books_clients.book_type_id where books_clients.day_diff > 0 order by full_fine DESC limit 1;
 
 /*Три самые популярные книги*/
  SELECT journal.book_id as book_id, count(journal.book_id) as count_books, books.name FROM library.journal as  journal
