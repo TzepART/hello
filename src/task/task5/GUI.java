f005f724090f9e7d85984134d82787cd725a35c3
tzepart <tzepart@gmail.com>
2017-12-19 18:01:03 +0300

    w_task_5_refactoring_step3

--- src/task/task5/GUI.java
+++ src/task/task5/GUI.java
@@ -54,188 +54,61 @@ public class GUI extends JFrame {
     private JScrollPane scroll = new JScrollPane(jta, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
     private JScrollPane scroll2 = new JScrollPane(jta2, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
 
-    // переменная для передачи параметров
-    String args;
 
     // контейнер для размещения компонентов формы
     private Container container = getContentPane();
 
-    //конструктор
-    GUI() {
-        // зададим размеры окна
-        super.setSize(830, 585);
+    protected String textTask1 = "1. Приложение с 3 параметрами. Необходимо определить  могут ли быть \n" +
+            "введенные параметры сторонами треугольника и вывести сообщение \n" +
+            "в случае, если это невозможно. \n\n" +
+            "Введите 3 аргумента через пробел в поле Введите аргументы и нажмите кнопку ОК.";
 
-        //заголовок
-        super.setTitle("Лабораторная работа №5");
+    protected String textTask2 = "2. Создать приложение с 2 классами- А, B  и 2 интерфейсами I1, I2.\n" +
+            "Класс А является родительским для класса B,\n" +
+            " Интерфейс I1 является родительским для интерфейса I2.\n" +
+            "Класс A реализует интерфейс I1.\n" +
+            "Класс В реализует интерфейс I2.\n" +
+            "Интерфейс I1 содержит метод i1, класс А  содержит метод а1, интерфейс I2 содержит \n" +
+            "метод i2. Класс B содержит метод b1.\n" +
+            "Все методы выводят строку с именем своего класса или интерфейса и именем метода.\n" +
+            "Создать минимальное число объектов для выполнения всех указанных 4 методов.\n" +
+            "и выполнить все эти методы. Присвоить переменным типа I1  каждый из созданных\n" +
+            " объектов и еще раз выполнить все прежние методы для\n" +
+            " переменной типа I1(используя приведение типов).";
 
-        //установим окно по центру экрана
-        super.setLocationRelativeTo(null);
+    protected String textTask3 = "3. Создать 2 потока разными способами. Один из этих потоков увеличивает разделенную\n" +
+            "между потоками переменную на 1000, а другой на 10 . Потоки работают попеременно.\n" +
+            "Цикл для каждого потока выполняется число раз заданное параметром.\n" +
+            "Нужно выводить для каждого потока его имя и значение измененной переменной.\n" +
+            "Выполнить задание с использованием конструкции synchronized .\n" +
+            "Не использовать в этом задании флаги для синхронизации потоков,\n" +
+            "а только методы wait и notify. Также не использовать любые задержки для\n" +
+            "потоков после начала их работы в виде методов sleep, yield или wait c параметром.\n\n" +
+            "Введите 2 аргумента через пробел:\n" +
+            "Первый - число циклов для 1го потока, \n" +
+            "Второй - число циклов для 2го потока\n" +
+            "и нажмите кнопку ОК.";
 
-        //инициализируем панели
-        initPanels();
-
-        //инициализируем заголовки
-        initLabels();
-
-        //кнопки RadioButton
-        initRadioButtons();
-
-        //установим параметры
-        GridBagConstraints gbs = new GridBagConstraints();
-        gbs.insets = new Insets(25, 0, 0, 0);
-        gbs.gridx = 0;
-        gbs.gridy = GridBagConstraints.RELATIVE;
-        gbs.anchor = GridBagConstraints.WEST;
-
-        //добавим RadioButtons к panel
-        panel.add(lab1, gbs);
-        panel.add(lab2, gbs);
-        panel.add(lab3, gbs);
-        panel.add(lab4, gbs);
-
-        // создадим ящик и будем укладывать в него элементы
-        Box bv = new Box(BoxLayout.Y_AXIS);
-
-        // минимальная ширина текстовых полей
-        bv.add(Box.createHorizontalStrut(60));
-
-        // рамка вокруг текстового поля
-        jta.setBorder(BorderFactory.createLineBorder(Color.black));
-
-
-        //добавим надпись на панель panel2
-        panel2.add(label1);
-
-        //установим размеры и цвет рамки scroll-панели и добавим ее на панель panel2
-        scroll2.setPreferredSize(new Dimension(610, 165));
-        scroll2.setBorder(BorderFactory.createLineBorder(Color.black));
-        scroll2.setAlignmentX(LEFT_ALIGNMENT);
-        panel2.add(scroll2);
-
-        // уберем панель panel2 в ящик
-        bv.add(panel2);
-        // пустое место в 15 пикселей
-        bv.add(Box.createVerticalStrut(15));
-
-        //добавим надпись на панель panel3
-        panel3.add(label2);
-
-        // установим цвет рамки и размеры поля ввода параметров
-        input.setPreferredSize(new Dimension(610, 30));
-        input.setBorder(BorderFactory.createLineBorder(Color.black));
-        input.setAlignmentX(LEFT_ALIGNMENT);
-        input.setDisabledTextColor(Color.GRAY);
-
-        //добавим надпись на поле ввода на панель panel3
-        panel3.add(input);
-
-        //зададим размеры кнопкам
-        initButtons();
-
-        //сделаем кнопки невидимыми
-        setInvisibleButtons();
-
-        // добавим кнопки на панель panel3
-        panel3.add(button1);
-        panel3.add(button2);
-        panel3.add(button3);
-        panel3.add(button4);
-
-        // уберем панель panel3 в ящик
-        bv.add(panel3);
-        bv.add(Box.createVerticalStrut(15));
-
-        // установим цвет рамки и размеры панели panel4
-        panel4.setBorder(BorderFactory.createLineBorder(Color.black));
-        panel4.setPreferredSize(new Dimension(60, 210));
-
-        //добавим надпись на поле ввода на панель panel4
-        panel4.add(label3);
-
-        //установим размеры и цвет рамки scroll-панели и добавим ее на панель panel4
-        scroll.setPreferredSize(new Dimension(610, 175));
-        scroll.setBorder(BorderFactory.createLineBorder(Color.black));
-        scroll.setAlignmentX(LEFT_ALIGNMENT);
-        panel4.add(scroll);
-
-        // уберем панель panel4 в ящик
-        bv.add(panel4);
-
-        // устанавливаем положение и добавляем ящик на main_panel
-        add(bv, BorderLayout.WEST);
-        main_panel.add(bv);
-
-        //установим размеры контейнера
-        container.setMinimumSize(new Dimension(830, 600));
-
-        //установим размеры для панелей и добавим их в контейнер
-        container.add(BorderLayout.WEST, panel);
-        container.add(BorderLayout.CENTER, main_panel);
-
-        //сделаем нередактируемыми поля для вывода задания и результатов работы программ
-        jta.setEditable(false);
-        jta2.setEditable(false);
-
-        // сделаем видимой фрейм
-        super.setVisible(true);
-        // запретим изменение параметров окна
-        super.setResizable(false);
-        super.toFront();
-    }
-
+    protected String textTask4 = "4. Создать приложение с 2 параметрами. 1 параметр задает количество cтудентов,\n" +
+            "2-ой параметр задает количество преподавателей.\n" +
+            "каждый преподаватель строго по очереди приглашает любого студента.\n" +
+            "Как только студент получает приглашение , то поток  студент заканчивается.\n" +
+            "Преподаватель становится после этого последним в очереди преподавателей на вызов студента .\n" +
+            "Потоки - преподаватели должны работать строго по очереди.\n" +
+            "Работа продолжается пока есть хоть один студент и если число преподавателей больше нуля.\n" +
+            "Использовать ограничения из задания 3.\n" +
+            "Выводить на дисплей имя преподавателя вместе с именем студента.\n" +
+            "Выглядеть это будет примерно так: преподаватель1- студент3\n" +
+            "преподаватель2- студент1\n" +
+            "преподаватель1- студент4\n" +
+            "преподаватель2- студент2\n\n" +
+            "Введите 2 аргумента через пробел:\n" +
+            "Первый - число преподавателей \n" +
+            "Второй - число студентов\n" +
+            "и нажмите кнопку ОК.";
 
     // Listener для отслеживания переключения radiobutton
     ActionListener listener = new ActionListener() {
-
-        private String textTask1 = "1. Приложение с 3 параметрами. Необходимо определить  могут ли быть \n" +
-                "введенные параметры сторонами треугольника и вывести сообщение \n" +
-                "в случае, если это невозможно. \n\n" +
-                "Введите 3 аргумента через пробел в поле Введите аргументы и нажмите кнопку ОК.";
-
-        private String textTask2 = "2. Создать приложение с 2 классами- А, B  и 2 интерфейсами I1, I2.\n" +
-                "Класс А является родительским для класса B,\n" +
-                " Интерфейс I1 является родительским для интерфейса I2.\n" +
-                "Класс A реализует интерфейс I1.\n" +
-                "Класс В реализует интерфейс I2.\n" +
-                "Интерфейс I1 содержит метод i1, класс А  содержит метод а1, интерфейс I2 содержит \n" +
-                "метод i2. Класс B содержит метод b1.\n" +
-                "Все методы выводят строку с именем своего класса или интерфейса и именем метода.\n" +
-                "Создать минимальное число объектов для выполнения всех указанных 4 методов.\n" +
-                "и выполнить все эти методы. Присвоить переменным типа I1  каждый из созданных\n" +
-                " объектов и еще раз выполнить все прежние методы для\n" +
-                " переменной типа I1(используя приведение типов).";
-
-        private String textTask3 = "3. Создать 2 потока разными способами. Один из этих потоков увеличивает разделенную\n" +
-                "между потоками переменную на 1000, а другой на 10 . Потоки работают попеременно.\n" +
-                "Цикл для каждого потока выполняется число раз заданное параметром.\n" +
-                "Нужно выводить для каждого потока его имя и значение измененной переменной.\n" +
-                "Выполнить задание с использованием конструкции synchronized .\n" +
-                "Не использовать в этом задании флаги для синхронизации потоков,\n" +
-                "а только методы wait и notify. Также не использовать любые задержки для\n" +
-                "потоков после начала их работы в виде методов sleep, yield или wait c параметром.\n\n" +
-                "Введите 2 аргумента через пробел:\n" +
-                "Первый - число циклов для 1го потока, \n" +
-                "Второй - число циклов для 2го потока\n" +
-                "и нажмите кнопку ОК.";
-
-        private String textTask4 = "4. Создать приложение с 2 параметрами. 1 параметр задает количество cтудентов,\n" +
-                "2-ой параметр задает количество преподавателей.\n" +
-                "каждый преподаватель строго по очереди приглашает любого студента.\n" +
-                "Как только студент получает приглашение , то поток  студент заканчивается.\n" +
-                "Преподаватель становится после этого последним в очереди преподавателей на вызов студента .\n" +
-                "Потоки - преподаватели должны работать строго по очереди.\n" +
-                "Работа продолжается пока есть хоть один студент и если число преподавателей больше нуля.\n" +
-                "Использовать ограничения из задания 3.\n" +
-                "Выводить на дисплей имя преподавателя вместе с именем студента.\n" +
-                "Выглядеть это будет примерно так: преподаватель1- студент3\n" +
-                "преподаватель2- студент1\n" +
-                "преподаватель1- студент4\n" +
-                "преподаватель2- студент2\n\n" +
-                "Введите 2 аргумента через пробел:\n" +
-                "Первый - число преподавателей \n" +
-                "Второй - число студентов\n" +
-                "и нажмите кнопку ОК.";
-
 
         @Override
         public void actionPerformed(ActionEvent ae) {
@@ -302,56 +175,130 @@ public class GUI extends JFrame {
         }
     };
 
+    //конструктор
+    GUI() {
+        // зададим размеры окна
+        super.setSize(830, 585);
+
+        //заголовок
+        super.setTitle("Лабораторная работа №5");
+
+        //установим окно по центру экрана
+        super.setLocationRelativeTo(null);
+
+        //инициализируем панели
+        initPanels();
+
+        //инициализируем заголовки
+        initLabels();
+
+        //кнопки RadioButton
+        initRadioButtons();
+
+        // создадим ящик и будем укладывать в него элементы
+        Box bv = new Box(BoxLayout.Y_AXIS);
+        // минимальная ширина текстовых полей
+        bv.add(Box.createHorizontalStrut(60));
+
+        //инициализируем отдельные поля
+
+        //поле2
+        initPanel2();
+        // уберем панель panel2 в ящик
+        bv.add(panel2);
+        // пустое место в 15 пикселей
+        bv.add(Box.createVerticalStrut(15));
+
+        //поле3
+        initPanel3();
+        // уберем панель panel3 в ящик
+        bv.add(panel3);
+        bv.add(Box.createVerticalStrut(15));
+
+        //поле4
+        initPanel4();
+        // уберем панель panel4 в ящик
+        bv.add(panel4);
+
+        // устанавливаем положение и добавляем ящик на main_panel
+        add(bv, BorderLayout.WEST);
+        main_panel.add(bv);
+
+        //установим размеры контейнера
+        container.setMinimumSize(new Dimension(830, 600));
+
+        //установим размеры для панелей и добавим их в контейнер
+        container.add(BorderLayout.WEST, panel);
+        container.add(BorderLayout.CENTER, main_panel);
+
+        // рамка вокруг текстового поля
+        jta.setBorder(BorderFactory.createLineBorder(Color.black));
+        //сделаем нередактируемыми поля для вывода задания и результатов работы программ
+        jta.setEditable(false);
+        jta2.setEditable(false);
+
+        // сделаем видимой фрейм
+        super.setVisible(true);
+        // запретим изменение параметров окна
+        super.setResizable(false);
+        super.toFront();
+    }
+
+
+    private void initPanel2() {
+        //добавим надпись на панель panel2
+        panel2.add(label1);
+
+        //установим размеры и цвет рамки scroll-панели и добавим ее на панель panel2
+        scroll2.setPreferredSize(new Dimension(610, 165));
+        scroll2.setBorder(BorderFactory.createLineBorder(Color.black));
+        scroll2.setAlignmentX(LEFT_ALIGNMENT);
+        panel2.add(scroll2);
+    }
+
+    private void initPanel3() {
+        //добавим надпись на панель panel3
+        panel3.add(label2);
+
+        // установим цвет рамки и размеры поля ввода параметров
+        input.setPreferredSize(new Dimension(610, 30));
+        input.setBorder(BorderFactory.createLineBorder(Color.black));
+        input.setAlignmentX(LEFT_ALIGNMENT);
+        input.setDisabledTextColor(Color.GRAY);
+
+        //добавим надпись на поле ввода на панель panel3
+        panel3.add(input);
+
+        //зададим размеры кнопкам
+        initButtons();
+
+        //сделаем кнопки невидимыми
+        setInvisibleButtons();
+
+        // добавим кнопки на панель panel3
+        panel3.add(button1);
+        panel3.add(button2);
+        panel3.add(button3);
+        panel3.add(button4);
+    }
+
+    private void initPanel4() {
+        //добавим надпись на поле ввода на панель panel4
+        panel4.add(label3);
+
+        // установим цвет рамки и размеры панели panel4
+        panel4.setBorder(BorderFactory.createLineBorder(Color.black));
+        panel4.setPreferredSize(new Dimension(60, 210));
+
+        //установим размеры и цвет рамки scroll-панели и добавим ее на панель panel4
+        scroll.setPreferredSize(new Dimension(610, 175));
+        scroll.setBorder(BorderFactory.createLineBorder(Color.black));
+        scroll.setAlignmentX(LEFT_ALIGNMENT);
+        panel4.add(scroll);
+    }
+
     static String[] getArrayArgumentsFromString(String string) {
         return string.split(" ");
-    }
-
-    protected void setTextToTaskTextField(String text) {
-        jta2.setText(text);
-        jta2.setCaretPosition(0);
-    }
-
-    private void setVisibleButton(int visibleButton) {
-        setInvisibleButtons();
-        switch (visibleButton) {
-            case 1:
-                button1.setVisible(true);
-                input.setEnabled(true);
-                label2.setForeground(Color.RED);
-                break;
-            case 2:
-                button2.setVisible(true);
-                input.setEnabled(false);
-                label2.setForeground(Color.GRAY);
-                break;
-            case 3:
-                button3.setVisible(true);
-                input.setEnabled(true);
-                label2.setForeground(Color.RED);
-                break;
-            case 4:
-                button4.setVisible(true);
-                input.setEnabled(true);
-                label2.setForeground(Color.RED);
-                break;
-            default:
-                break;
-        }
-
-    }
-
-    private void setInvisibleButtons() {
-        button1.setVisible(false);
-        button2.setVisible(false);
-        button3.setVisible(false);
-        button4.setVisible(false);
-    }
-
-
-    private void setEmptyFields() {
-        jta2.setText("");
-        input.setText("");
-        jta.setText("");
     }
 
     private void initButtons() {
@@ -436,5 +383,66 @@ public class GUI extends JFrame {
         group.add(lab2);
         group.add(lab3);
         group.add(lab4);
+
+        //установим параметры
+        GridBagConstraints gbs = new GridBagConstraints();
+        gbs.insets = new Insets(25, 0, 0, 0);
+        gbs.gridx = 0;
+        gbs.gridy = GridBagConstraints.RELATIVE;
+        gbs.anchor = GridBagConstraints.WEST;
+
+        //добавим RadioButtons к panel
+        panel.add(lab1, gbs);
+        panel.add(lab2, gbs);
+        panel.add(lab3, gbs);
+        panel.add(lab4, gbs);
+    }
+
+    protected void setTextToTaskTextField(String text) {
+        jta2.setText(text);
+        jta2.setCaretPosition(0);
+    }
+
+    private void setVisibleButton(int visibleButton) {
+        setInvisibleButtons();
+        switch (visibleButton) {
+            case 1:
+                button1.setVisible(true);
+                input.setEnabled(true);
+                label2.setForeground(Color.RED);
+                break;
+            case 2:
+                button2.setVisible(true);
+                input.setEnabled(false);
+                label2.setForeground(Color.GRAY);
+                break;
+            case 3:
+                button3.setVisible(true);
+                input.setEnabled(true);
+                label2.setForeground(Color.RED);
+                break;
+            case 4:
+                button4.setVisible(true);
+                input.setEnabled(true);
+                label2.setForeground(Color.RED);
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    private void setInvisibleButtons() {
+        button1.setVisible(false);
+        button2.setVisible(false);
+        button3.setVisible(false);
+        button4.setVisible(false);
+    }
+
+
+    private void setEmptyFields() {
+        jta2.setText("");
+        input.setText("");
+        jta.setText("");
     }
 }
